e124dd7b3626cb6600febdbcb1a68be1eddb6083
nlab-su <e@nlab.su>
2024-02-04 22:59:44 +0300

    edit

---
+++ debian11/.vagrant/bundler/global.sol
@@ -0,0 +1,1 @@
+{"dependencies":[["log4r",[">= 0"]],["concurrent-ruby",["~> 1.0"]],["i18n",[">= 0"]],["micromachine",[">= 2","< 4"]],["vagrant-vbguest",["= 0.32.0"]]],"checksum":"3362f1adde5bcc3a003328f368a257f188052c536b1ef653af9e2db04b696474","vagrant_version":"2.4.1"}
